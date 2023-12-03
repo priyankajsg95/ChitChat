6b7569a43527c95175dc8b5fc054c06948cfb6db
Priyanka Gupta <priyankagupta95653@gmail.com>
2023-12-03 22:21:54 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,52 @@
+import React, { useState, useEffect , useRef} from 'react';
+import './App.css';
+import { Button, TextField, List, ListItem, ListItemText, Paper, Typography } from '@mui/material';
+import io from 'socket.io-client';
+
+function App() {
+  const [messages, setMessages] = useState([]);
+  const [input, setInput] = useState('');
+  const socketRef = useRef(); // create a ref for socket
+
+  useEffect(() => {
+    socketRef.current = io('http://localhost:3000');
+  
+    socketRef.current.on('message', (message) => {
+      setMessages((messages) => {
+         // Log the updated state
+        return [...messages, message];
+      });
+    });
+  
+    return () => {
+      socketRef.current.disconnect();
+    };
+  }, []);
+
+  const sendMessage = (event) => {
+    event.preventDefault();
+    if (input) {
+      socketRef.current.emit('message', input); // use socket from the ref
+      setInput('');
+    }
+  };
+
+  return (
+    <Paper className="root">
+      <Typography variant="h4">Chat Application</Typography>
+      <List className="chatList">
+        {messages.map((message, index) => (
+          <ListItem key={index} className="chatMessage">
+            <ListItemText primary={ message }  />
+          </ListItem>
+        ))}
+      </List>
+      <form onSubmit={sendMessage}>
+        <TextField value={input} onChange={event => setInput(event.target.value)} fullWidth />
+        <Button type="submit" variant="contained" color="primary" style={{ marginTop: '20px' }}>Send Message</Button>
+      </form>
+    </Paper>
+  );
+}
+
+export default App;
---
+++ src/server.js
@@ -0,0 +1,34 @@
+// server.js
+const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
+const readline = require('readline');
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+io.on('connection', (socket) => {
+    console.log('New client connected');
+  
+    // Send a welcome message to the new client
+    socket.emit('message', 'Welcome to the chat!');
+  
+    socket.on('message', (message) => {
+      console.log('Received message:', message); // Log received message
+      io.emit('message', message);
+    });
+  
+    socket.on('disconnect', () => {
+      console.log('Client disconnected');
+    });
+  });
+  
+  server.listen(3000, () => console.log('Server listening on port 3000'));
+
+  rl.on('line', (input) => {
+    io.emit('message', input);
+  });
